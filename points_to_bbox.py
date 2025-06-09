78eff72e9ba77ebcc3389ac98d7910a12b4186ff
Matthieu Chourrout <83714683+chourroutm@users.noreply.github.com>
2025-06-09 22:58:51 +0100

    Add margin

--- points_to_bbox.py
+++ points_to_bbox.py
@@ -6,11 +6,11 @@ import neuroglancer.cli
 import neuroglancer.random_token
 import numpy as np
 
-def _bounding_box(points):
+def _bounding_box_with_margin(points, margin=0):
     x_coordinates, y_coordinates, z_coordinates = zip(*points)
-    return [(min(x_coordinates), min(y_coordinates), min(z_coordinates)), (max(x_coordinates), max(y_coordinates), max(z_coordinates))]
+    return [(min(x_coordinates) - margin, min(y_coordinates) - margin, min(z_coordinates) - margin), (max(x_coordinates) + margin, max(y_coordinates) + margin, max(z_coordinates) + margin)]
 
-def convert_points_to_bbox(state: neuroglancer.ViewerState, keep_points_layer: bool = False, annotation_layer_suffix: str = "_bbox"):
+def convert_points_to_bbox(state: neuroglancer.ViewerState, keep_points_layer: bool = False, annotation_layer_suffix: str = "_bbox", margin = 0):
     for current_layer in state.layers:
         if not current_layer.visible:
             continue
@@ -31,11 +31,12 @@ def convert_points_to_bbox(state: neuroglancer.ViewerState, keep_points_layer: b
             new_layer.source[0].transform = default_transform
         else:
             new_layer.source[0].transform = current_layer.layer.source[0].transform
+        
         new_layer.annotations.append(
             neuroglancer.AxisAlignedBoundingBoxAnnotation(
                     id=neuroglancer.random_token.make_random_token(),
-                    point_a= _bounding_box(all_points)[0],
-                    point_b= _bounding_box(all_points)[1],
+                    point_a= _bounding_box_with_margin(all_points,margin=margin)[0],
+                    point_b= _bounding_box_with_margin(all_points)[1],
                 )
         )
         if keep_points_layer:
@@ -54,10 +55,12 @@ def main(args=None):
     neuroglancer.cli.add_state_arguments(ap, required=True)
     ap.add_argument("--keep_points", "-k", "--keep-points", action="store_true", help="Keep points layer instead of replacing it")
     ap.add_argument("--suffix", "-s", type=str, default="_bbox", help="Suffix for the new annotation layer")
+    ap.add_argument("--margin", "-m", type=int, default=0, help="Add margin around the bounding box")
     parsed_args = ap.parse_args()
     new_state = convert_points_to_bbox(state=parsed_args.state, 
                                        keep_points_layer=parsed_args.keep_points, 
                                        annotation_layer_suffix=parsed_args.suffix,
+                                       margin=parsed_args.margin,
                                     )
     print(neuroglancer.to_url(new_state))
 
